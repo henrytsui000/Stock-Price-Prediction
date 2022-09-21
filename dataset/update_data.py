58e909cc390c026ecce7d172e125058dc47d37dd
henrytsui000 <henrytsui000@gmail.com>
2022-09-21 14:10:08 +0800

    add requirement | move make data to /dateset

--- dataset/update_data.py
+++ dataset/update_data.py
@@ -1,27 +1,33 @@
-from tabnanny import check
 import pandas as pd
 import ffn
 import os
-import datetime
+import datetime as dt
+import parser
 
 # table = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
 # print(table)
 PATH = "./src/stock_data/stock.csv"
 
+
 def check_dir():
     if not os.path.isdir("./src"): os.mkdir("src")
     if not os.path.isdir("./src/stock_data"): os.mkdir("src/stock_data")
 
-def read_data():
-    if not os.path.isfile("./src/stock_data/stock.csv"):
-        pass
-    else :
+def read_data(start_day = dt.datetime(2022, 9, 10), stock_name = ""):
+    stock_name = "SPY:Open,SPY:High,SPY:Low,SPY:Close,SPY"
+    if os.path.isfile(PATH):
         stock = pd.read_csv(PATH)
-    nowday = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    endday = stock.index[-1]
-    if (nowday > endday) :
-        # 補日子
-    
+    else :
+        stock = ffn.get(stock_name, start = start_day)
+
+    print(type(stock))
+    # nowday = dt.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    # endday = stock.index[-1]
+    # if (nowday > endday) :
+    #     # 補日子
+    #     stock = ffn.get("SPY:Open,SPY:High,SPY:Low,SPY:Close,SPY", start=endday,)
+
+    stock.to_csv(PATH)
 
     
 def main():
