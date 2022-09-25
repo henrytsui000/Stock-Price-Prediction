d5c4557b334e67b01ab1c42a426a89dcdbbc998e
henrytsui000 <henrytsui000@gmail.com>
2022-09-25 18:16:28 +0800

    move file to corresponding location

--- dataset/update_data.py
+++ dataset/update_data.py
@@ -1,26 +1,18 @@
-from ast import parse
-from operator import index, indexOf
-from xml.sax import make_parser
-import pandas as pd
 import ffn
 import os
+import pandas as pd
 import datetime as dt
 from argparse import ArgumentParser
-
-# table = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-# print(table)
 
 def make_parser():
     parser = ArgumentParser()
     parser.add_argument("-p", "--path", type = str, default="./src/stock_data/stock.csv")
     parser.add_argument("-s", "--stock",type = str, default="SPY:Open,SPY:High,SPY:Low,SPY:Close,SPY" )
-
     parser.add_argument('-d', '--date', type = lambda s: dt.datetime.strptime(s, '%Y-%m-%d'), 
                                             default = dt.datetime(2022, 9, 10))
-
     return parser
 
-def check_dir(args):
+def check_dir():
     if not os.path.isdir("./src"): os.mkdir("src")
     if not os.path.isdir("./src/stock_data"): os.mkdir("src/stock_data")
 
@@ -35,9 +27,8 @@ def read_data(args):
 
     stock.to_csv(args.path, index=True)
 
-    
 def main(args):
-    check_dir(args)
+    check_dir()
     read_data(args)
 
 if __name__ == "__main__":
