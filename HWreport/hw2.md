16673b3851212c1faa52d1c9c9f7c11439cacd92
henrytsui000 <henrytsui000@gmail.com>
2022-10-02 22:44:02 +0800

    add hw2 file

---
+++ HWreport/hw2.md
@@ -0,0 +1,10 @@
+Data Science HW2
+===
+
+# Introduction
+First of all, I choose to use markdown syntax to write this assignment, because in this assignment, I use python to analyze the relevant content of the assignment. The word format will make the entire layout very confusing when you put the code, and you cannot put dynamic files such as gif or mp4. In this case, I think MarkDown is more suitable (previously, I sent a letter to ask the professor if the format is not restricted)
+
+
+
+# FAANG
+
--- tools/update_data.py
+++ tools/update_data.py
@@ -1,3 +1,4 @@
+from ast import parse
 import ffn
 import os
 import pandas as pd
@@ -6,18 +7,18 @@ from argparse import ArgumentParser
 
 def make_parser():
     parser = ArgumentParser()
-    parser.add_argument("-p", "--path", type = str, default="./src/stock_data/stock.csv")
-    parser.add_argument("-s", "--stock",type = str, default="META, AMZN, AAPL, NFLX, GOOG" )
+    parser.add_argument("-p", "--path", type = str, default="./data/stock.csv")
+    parser.add_argument("-s", "--stock",type = str, default="META, GOOG, AMZN, NFLX, AAPL" )
+    parser.add_argument("-f", "--force", action="store_false")
     parser.add_argument('-d', '--date', type = lambda s: dt.datetime.strptime(s, '%Y-%m-%d'), 
-                                            default = dt.datetime(2022, 9, 10))
+                                            default = dt.datetime(2012, 1, 1))
     return parser
 
 def check_dir():
-    if not os.path.isdir("./src"): os.mkdir("src")
-    if not os.path.isdir("./src/stock_data"): os.mkdir("src/stock_data")
+    if not os.path.isdir("./data"): os.mkdir("./data")
 
 def read_data(args):
-    if os.path.isfile(args.path):
+    if os.path.isfile(args.path) and args.force :
         stock = pd.read_csv(args.path, index_col = 0).round(4)
         stock.index = pd.to_datetime(stock.index)
         new_stock = ffn.get(args.stock, start = stock.index[-1],).round(4)
