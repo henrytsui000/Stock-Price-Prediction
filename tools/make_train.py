8fd8c064a038aa231d2d6e4b9a68cbaf22489c9a
LucyTuan <yztuan1129@gmail.com>
2022-11-26 03:52:44 +0800

    :sparkles: Upgrade make_train.py and .ipynb
    With new feature

--- tools/make_train.py
+++ tools/make_train.py
@@ -7,21 +7,26 @@ import time
 news = pd.read_csv("./data/news.csv")
 price = pd.read_csv("./data/stock.csv")
 
-merge = pd.DataFrame(columns=["symbol", "date", "time","content", "preprice", "latprice", "match", "sentiment", "score"])
+revised_news = pd.DataFrame(columns=[["symbol", "datentime", "content", "match", "sentiment", "score"]])
+for title in ["symbol","content", "match", "sentiment"]:
+    revised_news[title] = news[title]
+revised_news["score"] = news["match"] * news["sentiment"]
+revised_news["datentime"] = pd.to_datetime(news['date']) + pd.to_timedelta(news['time'])
 
-for title in ["symbol", "date", "time", "content", "match", "sentiment"]:
-    merge[title] = news[title]
-merge["score"] = news["match"] * news["sentiment"]
 
 pzmerge = pd.DataFrame()
+usdatecol = pd.DataFrame()
 for idx, new in tqdm(news.iterrows()):
     date = new["date"]
-    time = new["time"]
+    times = new["time"]
     match =new["match"]
     senti = new["sentiment"]
-    cb = date +" "+ time
+    cb = date +" "+ times
     usdate = dt.datetime.strptime(cb, '%Y-%m-%d %H:%M:%S')
-    usdate -= dt.timedelta(hours=5)
+    usdate += dt.timedelta(hours=3)
+    usdateindex = dt.datetime.strftime(usdate,'%Y-%m-%d %H:%M:%S')
+    usdateindex = pd.Series(usdateindex)
+    usdatecol = pd.concat([usdatecol, usdateindex])
     symbol = new["symbol"].lower()
     symbol = symbol if not symbol == "googl" else "goog"
     dtformat = dt.datetime.strptime(date, '%Y-%m-%d').date()
@@ -32,19 +37,19 @@ for idx, new in tqdm(news.iterrows()):
             dtformat+=dt.timedelta(days=1)
             date = dt.datetime.strftime(dtformat,'%Y-%m-%d')
     row = price[price["Date"]==date]
-    if usdate.hour<16:
-        nowprice = price.iloc[row.index][[symbol]]
-        preprice = price.iloc[row.index-1][[symbol]]
-        nextprice = price.iloc[row.index+1][[symbol]]
-    else:
-        preprice = price.iloc[row.index][[symbol]]
-        nowprice = price.iloc[row.index+1][[symbol]]
-        nextprice = price.iloc[row.index+2][[symbol]]
+    nowprice = price.iloc[row.index][[symbol]]
+    preprice = price.iloc[row.index-1][[symbol]]
+    nextprice = price.iloc[row.index+1][[symbol]]
     pz = pd.concat([preprice,nowprice,nextprice]).T
     pz.columns = ["preprice","nowprice","nextprice"]
     pzmerge = pd.concat([pzmerge, pz])
 
-df = news.reset_index(drop=True)
+usdatecol.reset_index(level=0, inplace=True)
+revised_news["datentime"] = usdatecol[0]
+revised_news.to_csv("./data/revised_news.csv")
+revised_news = pd.read_csv("./data/revised_news.csv",index_col=0)
+df = revised_news.reset_index(drop=True)
 df1 = pzmerge.reset_index(drop=True)
 merge = pd.concat([df,df1], axis = 1)
-merge.to_csv("./data/merge2.csv")
+merge.to_csv("./data/bertdataset6.csv")
+
